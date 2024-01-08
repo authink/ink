4f82fe3a7a9b27bb7b01ffdc424250819bd6e3a3
huoyijie <yijie.huo@foxmail.com>
2024-01-08 17:31:08 +0800

    add shutting down logic

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,5 +22,32 @@ func main() {
 			"message": "pong",
 		})
 	})
-	r.Run(fmt.Sprintf("127.0.0.1:%s", port))
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf("127.0.0.1:%s", port),
+		Handler: r,
+	}
+
+	go func() {
+		// 服务连接
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	// 等待中断/运行终止信号，优雅地关闭服务器
+	<-quit
+	log.Println("Shutting down ...")
+
+	// 设置 5 秒的超时时间
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// 在超时时间内停止服务
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown: %s\n", err)
+	}
 }
