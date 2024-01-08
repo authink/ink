4328a345bf5132e5c5ab2d3d589f2541fcfbca89
huoyijie <yijie.huo@foxmail.com>
2024-01-08 12:08:22 +0800

    change port

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+	r.Run(fmt.Sprintf(":%s", port))
 }
