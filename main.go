df048da1d8833e6eb723088eb293a8523a1c7ca4
huoyijie <yijie.huo@foxmail.com>
2024-01-08 12:12:21 +0800

    change pong

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "pong=>",
 		})
 	})
 	r.Run(fmt.Sprintf(":%s", port))
