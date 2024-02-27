ab5d32768a5fdb5564a614e71b8e845636c5276e
huoyijie <yijie.huo@foxmail.com>
2024-02-27 12:15:11 +0800

    add update staff tests

--- src/router/admin/admin_test.go
+++ src/router/admin/admin_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/authink/ink.go/src/router/token"
 	"github.com/authink/inkstone"
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 )
 
 var ctx = context.Background()
@@ -24,11 +25,22 @@ func TestMain(m *testing.M) {
 		&i18n.Locales,
 		func(appContext *inkstone.AppContext) {
 			migrate.Seed(appContext)
-			err := orm.App(appContext).Save(model.NewApp(
-				"devtools",
-				"123456",
-			))
-			if err != nil {
+			if err := inkstone.Transaction(appContext, func(tx *sqlx.Tx) (err error) {
+				if err = orm.App(appContext).SaveWithTx(model.NewApp(
+					"devtools",
+					"123456",
+				), tx); err != nil {
+					return
+				}
+
+				err = orm.Staff(appContext).SaveWithTx(model.NewStaff(
+					"test@huoyijie.cn",
+					"123456",
+					"11111111111",
+					false,
+				), tx)
+				return
+			}); err != nil {
 				panic(err)
 			}
 		},
--- src/router/admin/staff_test.go
+++ src/router/admin/staff_test.go
@@ -1,6 +1,7 @@
 package admin
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -58,7 +59,44 @@ func TestAddStaff(t *testing.T) {
 	resAddStaff := new(staffRes)
 	w2, _ := tAddStaff(resObj.AccessToken, "example@huoyijie.cn", "18555201314", false, resAddStaff)
 	assert.Equal(t, http.StatusOK, w2.Code)
-	assert.Equal(t, 100001, resAddStaff.Id)
+	assert.Equal(t, 100002, resAddStaff.Id)
 	assert.Equal(t, "example@huoyijie.cn", resAddStaff.Email)
 	assert.NotEmpty(t, resAddStaff.Password)
+}
+
+func tUpdateStaff(accessToken string, id int, reqObj, resObj any) (*httptest.ResponseRecorder, error) {
+	return inkstone.TestFetch(
+		ctx,
+		"PUT",
+		fmt.Sprintf("admin/staffs/%d", id),
+		reqObj,
+		resObj,
+		accessToken,
+	)
+}
+
+func TestUpdateStaff(t *testing.T) {
+	resObj := new(token.GrantRes)
+	w, _ := grantToken(100000, "123456", "admin@huoyijie.cn", "123456", resObj)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.NotEmpty(t, resObj.AccessToken)
+	assert.NotEmpty(t, resObj.RefreshToken)
+
+	updateReq := &updateStaffReq{
+		Phone:           "12112112112",
+		ActiveToggle:    true,
+		DepartureToggle: true,
+		SuperToggle:     true,
+		ResetPassword:   true,
+	}
+	updateRes := new(staffRes)
+	w2, _ := tUpdateStaff(resObj.AccessToken, 100001, updateReq, updateRes)
+	assert.Equal(t, http.StatusOK, w2.Code)
+	assert.Equal(t, 100001, updateRes.Id)
+	assert.Equal(t, "12112112112", updateRes.Phone)
+	assert.NotEqual(t, "123456", updateRes.Password)
+	assert.False(t, updateRes.Active)
+	assert.True(t, updateRes.Super)
+	assert.True(t, updateRes.Departure)
 }
